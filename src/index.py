1b432774aa030a79988d19f8f6255041756201b8
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-23 10:00:34 +0700

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
 
 from src.dtos.ISayHelloDto import ISayHelloDto
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import os
-import json
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+
 app = FastAPI()
 
 
