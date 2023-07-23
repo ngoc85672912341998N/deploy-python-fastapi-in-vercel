f161671cb441fe7726f57b71af350f546812b2e5
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-23 10:23:28 +0700

    Update index.py

--- src/index.py
+++ src/index.py
@@ -2,12 +2,28 @@ from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-def write_notification(email: str, message=""):
-    with open("log.txt", mode="w") as email_file:
-        content = f"notification for {email}: {message}"
-        email_file.write(content)
 
+def write_notification(url: str, message=""):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    link_avartar=[]
+    time.sleep(5)
+    value = driver.find_elements(By.TAG_NAME, 'video')
+    for value in value:
+        print(value.get_attribute("data-src"))
+        k3=value.get_attribute("data-src")
+        k10=str(k3)
+        k= len(k10.split("/"))
+        print(k10.split("/")[k-1].split(".")[0])
+        k5= str(k10.split("/")[k-1].split(".")[0])
+        k4 =str(k5)+","+str(k3)+"\n"
 
 @app.post("/send-notification/{email}")
 async def send_notification(email: str, background_tasks: BackgroundTasks):
