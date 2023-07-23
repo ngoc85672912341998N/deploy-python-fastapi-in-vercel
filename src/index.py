6bf1401152d6e69f84edac5be77d93f7696fa0cd
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-23 10:29:02 +0700

    aa

--- src/index.py
+++ src/index.py
@@ -7,23 +7,11 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-
+from selenium import webdriver
+import chromedriver_autoinstaller
 def write_notification(url: str, message=""):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-    link_avartar=[]
-    time.sleep(5)
-    value = driver.find_elements(By.TAG_NAME, 'video')
-    for value in value:
-        print(value.get_attribute("data-src"))
-        k3=value.get_attribute("data-src")
-        k10=str(k3)
-        k= len(k10.split("/"))
-        print(k10.split("/")[k-1].split(".")[0])
-        k5= str(k10.split("/")[k-1].split(".")[0])
-        k4 =str(k5)+","+str(k3)+"\n"
+    driver = webdriver.Chrome()
+    driver.get("http://www.python.org")
 
 @app.post("/send-notification/{email}")
 async def send_notification(email: str, background_tasks: BackgroundTasks):
