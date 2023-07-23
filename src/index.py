7cffbf7eabf4ec442d64dc8f3fda73ffe41f1351
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-23 09:58:15 +0700

    Update index.py

--- src/index.py
+++ src/index.py
@@ -13,28 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 app = FastAPI()
 
-def selenium():
-    BROWSERSTACK_USERNAME = "tranngocminh_AFiWRo"
-    BROWSERSTACK_ACCESS_KEY = "8anY1yMSZhs7qsHCzLeD"
-    URL = "https://hub.browserstack.com/wd/hub"
 
-    bstack_options = {
-        "os": "OS X",
-        "osVersion": "Monterey",
-        "buildName": "browserstack-build-1",
-        "sessionName": "BStack single python",
-        "userName": BROWSERSTACK_USERNAME,
-        "accessKey": BROWSERSTACK_ACCESS_KEY
-    }
-    bstack_options["source"] = "python:sample-main:v1.0"
-    options = ChromeOptions()
-    options.set_capability('bstack:options', bstack_options)
-    driver = webdriver.Remote(
-        command_executor=URL,
-        options=options)
-    driver.get("https://www.synthesia.io/features/avatars")
-    driver.quit()
-    print("test")
 @app.get("/")
 async def root():
     return {"message": "xin chào bạn"}
